1d63e6e2c3341baa968c7347c755d075728a2732
tp6m4fu6250071 <tp6m4fu6250071@gmail.com>
2020-01-17 13:45:03 +0800

    add logs

--- alb_support/recipes/attach_to_alb.rb
+++ alb_support/recipes/attach_to_alb.rb
@@ -19,20 +19,27 @@ chef_gem "aws-sdk-core" do
   version "~> 2.6"
   action :install
 end
-
+Chef::Log.info('attach_alb-000')
 ruby_block "attach to ALB" do
   block do
+    Chef::Log.info('attach_alb-001')
     require "aws-sdk-core"
 
     raise "alb_helper block not specified in layer JSON" if node[:alb_helper].nil?
     raise "Target group ARN not specified in layer JSON" if node[:alb_helper][:target_group_arn].nil?
 
+    Chef::Log.info('attach_alb-002')
+    
     stack = search("aws_opsworks_stack").first
     instance = search("aws_opsworks_instance", "self:true").first
+
+    Chef::Log.info('attach_alb-003')
 
     stack_region = stack[:region]
     ec2_instance_id = instance[:ec2_instance_id]
     target_group_arn = node[:alb_helper][:target_group_arn]
+
+    Chef::Log.info('attach_alb-004')
 
     Chef::Log.info("Creating ELB client in region #{stack_region}")
     client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
@@ -44,7 +51,12 @@ ruby_block "attach to ALB" do
       targets: [{ id: ec2_instance_id }]
     }
 
+    Chef::Log.info('attach_alb-005')
+
     client.register_targets(target_to_attach)
+    Chef::Log.info('attach_alb-006')
   end
   action :run
 end
+
+Chef::Log.info('attach_alb-007')
--- alb_support/recipes/detach_from_alb.rb
+++ alb_support/recipes/detach_from_alb.rb
@@ -15,12 +15,20 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 #
+
+Chef::Log.info('detach_alb-000')
+
 ruby_block "detach from ALB" do
   block do
+    Chef::Log.info('detach_alb-001')
     require "aws-sdk-core"
+
+    Chef::Log.info('detach_alb-002')
 
     raise "alb_helper block not specified in layer JSON" if node[:alb_helper].nil?
     raise "Target Group ARN not specified in layer JSON" if node[:alb_helper][:target_group_arn].nil?
+
+    Chef::Log.info('detach_alb-003')
 
     connection_draining_timeout = node[:alb_helper][:connection_draining_timeout]
     state_check_frequency = node[:alb_helper][:state_check_frequency]
@@ -31,12 +39,16 @@ ruby_block "detach from ALB" do
     stack = search("aws_opsworks_stack").first
     instance = search("aws_opsworks_instance", "self:true").first
 
+    Chef::Log.info('detach_alb-004')
+
     stack_region = stack[:region]
     ec2_instance_id = instance[:ec2_instance_id]
     target_group_arn = node[:alb_helper][:target_group_arn]
 
     Chef::Log.info("Creating ELB client in region #{stack_region}")
     client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
+
+    Chef::Log.info('detach_alb-005')
 
     target_to_detach = {
       target_group_arn: target_group_arn,
@@ -47,11 +59,14 @@ ruby_block "detach from ALB" do
     client.deregister_targets(target_to_detach)
 
     if connection_draining_timeout == 0
+      Chef::Log.info('detach_alb-006-1')
       Chef::Log.info("connection_draining_timeout was set to 0 seconds. execution of shutdown recipes will not be delayed")
     else
+      Chef::Log.info('detach_alb-006-2')
       Chef::Log.info("delaying execution recipes until instance is drained from ALB or timeout of #{connection_draining_timeout} seconds elapses")
       start_time = Time.now
       loop do
+        Chef::Log.info('detach_alb-007')
         response = client.describe_target_health(target_to_detach)
         target_health_state = response[:target_health_descriptions].first[:target_health][:state]
         Chef::Log.info("state of instance in ALB: #{target_health_state}")
@@ -61,7 +76,10 @@ ruby_block "detach from ALB" do
         break if target_health_state == "unused" || seconds_elapsed > connection_draining_timeout
         sleep(state_check_frequency)
       end
+      Chef::Log.info('detach_alb-008')
     end
+    Chef::Log.info('detach_alb-009')
   end
   action :run
 end
+Chef::Log.info('detach_alb-010')
